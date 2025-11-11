9edb1988278c02c0260b936041696112e4340272
lucasojeda <wichisan@hotmail.com>
2025-11-11 18:22:21 -0300

    trabajo2

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -28,7 +28,7 @@ class AuthService {
             html: `
                 <h1>Hola ${name}</h1>
                 <p>Verifica tu correo electrónico:</p>
-                <a href='${ENVIRONMENT.URL_API_BACKEND}/api/auth/verify-email/${verification_token}'>Verificar email</a>
+                <a href='${ENVIRONMENT.URL_FRONTEND}/api/auth/verify-email/${verification_token}'>Verificar email</a>
             `
                 
         });
