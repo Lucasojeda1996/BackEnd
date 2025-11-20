c529c4099cc5321e21badeaf39caf53eaeaf2c64
lucasojeda <wichisan@hotmail.com>
2025-11-20 15:38:56 -0300

    asd

--- src/models/Channel.model.js
+++ src/models/Channel.model.js
@@ -5,7 +5,7 @@ const channelSchema = new mongoose.Schema({
         type:String,
         required: true
     },
-    workspace_ida: {
+    workspace_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Workspace",
         required: true
